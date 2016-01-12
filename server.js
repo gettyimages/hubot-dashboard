fa61e0757d4e09c6abb4b62aabf6fcb692a2aabb
Casey MacPherson <casey.macpherson@gettyimages.com>
2016-01-11 20:47:59 -0800

    Adding bower bindings, tests, and initial home page

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-module.export = function(robot) {
+module.exports = function(robot) {
     var express = require('express')
     var app = express()
     var server = require('http').Server(app);
     var io = require('socket.io')(server);
     var jade = require('jade')
 
+    //Going to add a logging interceptor
+    var infoLogger = robot.logger.info
+    robot.logger.info = function(message) {
+        io.emit('newLogUpdate',message)
+        infoLogger(message)    
+    }
+    
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.set("view options", { layout: false });
     app.use(express.static(__dirname + '/public'));
 
     app.get('/', function(req, res){
-    res.render('home.jade');
+        res.render('home.jade');
     });
 
     server.listen(3000);
 
     io.on('connection', function (socket) {
-        console.log(new Date() + "New Connection from client")
+        robot.logger.info("new connection")
         socket.emit('connected')
     });
-    
-    //Going to add a logging interceptor
-    var originalLogger = robot.log
-    robot.log = function(message) {
-        io.emit('newLogUpdate',message)
-        originalLogger(message)    
-    }
 }
---
+++ test/log-emitting.js
@@ -0,0 +1,42 @@
+var expect = require('chai').expect
+var mockRobot = require('mock-hubot-robot')
+var client = require('socket.io-client')
+
+describe('Logging interception',function() {
+    var server = require('../server.js')
+    var robotCtx = mockRobot()
+    var socket;
+    
+    before(function(done) { 
+       //Unhooks the 2000 ms default timeout in Mocha
+       this.timeout(0)
+       //Starts Server
+       server(robotCtx)
+       
+       //Connects our client
+       socket = client.connect("http://localhost:3000")
+       
+       //Waits for connection before continuing
+       socket.on('connected',function() {
+           done()
+       })
+    })
+    
+    it('should emit logging',function(done) {        
+        socket.on('newLogUpdate',function(data) {
+           console.log(data)
+           expect(data).to.be.equal("test")
+           done()
+        })
+        
+        //fires a logging message
+        robotCtx.logger.info("test")
+    })
+    
+    it('should allow the log to continue to the standard robot logger', function() {
+        var itemsLogged = robotCtx.getLogItems().info
+        expect(itemsLogged).not.to.be.null
+        expect(itemsLogged.length).to.be.equal(2)
+        expect(itemsLogged).to.contain("test")
+    })
+})
